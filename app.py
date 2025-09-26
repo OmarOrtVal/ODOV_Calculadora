f18f3f8bedcef51f1ea3ebf72766e9aaae9bc292
OmarOrtVal <23308060610674@cetis61.edu.mx>
2025-09-26 11:01:11 -0600

    trabajo

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def hola_mundo():
-    return '<h1>¡Hola, Mundo desde Flask!<h1>'
+    return '<h1>¡Hola Mundo desde Flask!<h1>'
 
 if __name__ == '__main__':
     app.run(debug=True)
