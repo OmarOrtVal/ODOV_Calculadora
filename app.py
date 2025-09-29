6b151c08ac5c7e0b2e55e3111a7c258436da138f
OmarOrtVal <23308060610674@cetis61.edu.mx>
2025-09-29 09:15:23 -0600

    se convirtio a calculadora

--- app.py
+++ app.py
@@ -3,22 +3,81 @@ from flask import Flask
 app = Flask(__name__)
 
 @app.route('/')
-def hola_mundo(mensaje):
+def calculadora():
     mensaje = '''
-    <h1>¡Hola Mundo desde Flask!<h1>
-    <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Animi, maiores! Nesciunt quos repellat voluptatibus vero sunt obcaecati voluptatum magnam quo illum, rerum tempore architecto, illo asperiores maiores odit non labore.</p>
-    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Optio adipisci, itaque nostrum consectetur eveniet doloribus culpa quam, molestias, quidem quasi distinctio doloremque nisi aliquid harum dignissimos beatae sequi totam? Ipsa!</p>
-    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Veniam, qui autem illum, perspiciatis cumque tempora nam ullam possimus deleniti perferendis officia quibusdam! Laboriosam nobis impedit soluta, saepe quos aliquid quasi.</p>
-    <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Distinctio quibusdam explicabo placeat labore sed non, laboriosam ipsum eveniet quod excepturi culpa amet facilis itaque ad quasi accusantium? Corporis, omnis quas!</p>
-    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Repudiandae optio nihil fugiat non facere. Reprehenderit, hic. Esse sint inventore nemo dolorum dicta, molestias, commodi nobis ad obcaecati impedit, laboriosam nam?</p>
-    '''
+    <h1>Bienvenido a la calculadora de Python<h1>
+    <h2>Para sumar escribe en el navegador http://127.0.0.1:5000/sumar <h2>
+    <h2>Para restar escribe en el navegador http://127.0.0.1:5000/restar<h2>
+    <h2>Para multiplicacion escribe en el navegador http://127.0.0.1:5000/multiplicacion<h2>
+    <h2>Para division escribe en el navegador http://127.0.0.1:5000/division<h2>
+    <h2>Para mayor escribe en el navegador http://127.0.0.1:5000/mayor<h2>
+    <h2>Para menor escribe en el navegador http://127.0.0.1:5000/menor<h2>
+    <h2> DESPUES DE EL NOMBRE DE LA OPERACION DEBES PONER LOS 2 NUMEROS QUE DESEAS UTILIZAR PARA TU OPREACION
+    EJEMPLO:http://127.0.0.1:5000/sumar/5/42 <h2>
 
-@app.route('/factorial/<v1>')
-def factorial(v1):
-    f=1
-    for x in range(1,int(v1)+1):
-        f*=x
-    return (f"El factorial de {v1}! es {f}")
+    <footer>
+    OMAR DANIEL ORTEGA VALTIERRA
+    23308060610674
+    5D
+    PROGRAMACION
+    </footer>
+    '''
+    return mensaje
+
+@app.route('/sumar/<v1>/<v2>')
+def sumar(v1, v2):
+    num1 = int(v1)
+    num2 = int(v2)
+    suma = num1 + num2
+    return (f"La suma de {v1} y {v2} es {suma}")
+
+@app.route('/restar/<v1>/<v2>')
+def restar(v1, v2):
+    num1 = int(v1)
+    num2 = int(v2)
+    resta = num1 - num2
+    return (f"La resta de {v1} y {v2} es {resta}")
+
+@app.route('/multiplicacion/<v1>/<v2>')
+def multiplicacion(v1, v2):
+    num1 = int(v1)
+    num2 = int(v2)
+    multiplicacion = num1 * num2
+    return (f"La multiplicacion de {v1} y {v2} es {multiplicacion}")
+
+@app.route('/division/<v1>/<v2>')
+def division(v1, v2):
+    num1 = int(v1)
+    num2 = int(v2)
+    dividir = num1 / num2
+    return (f"La division de {v1} y {v2} es {dividir}")
+
+@app.route('/mayor/<v1>/<v2>')
+def mayor(v1, v2):
+    num1 = int(v1)
+    num2 = int(v2)
+    if num1 > num2:
+        mayor_numero = num1
+        return (f"El mayor número entre {v1} y {v2} es {mayor_numero}")
+    elif num2 > num1:
+        mayor_numero = num2
+        return (f"El mayor número entre {v1} y {v2} es {mayor_numero}")
+    else:
+        return (f"Los números {v1} y {v2} son iguales")
+    
+@app.route('/menor/<v1>/<v2>')
+def menor(v1, v2):
+    num1 = int(v1)
+    num2 = int(v2)
+    if num1 < num2:
+        menor_numero = num1
+        return (f"El menor número entre {v1} y {v2} es {menor_numero}")
+    elif num2 < num1:
+        menor_numero = num2
+        return (f"El menor número entre {v1} y {v2} es {menor_numero}")
+    else:
+        return (f"Los números {v1} y {v2} son iguales")
+    
 
 if __name__ == '__main__':
     app.run(debug=True)
